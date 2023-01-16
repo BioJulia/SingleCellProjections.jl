15a6a7274c10588236b2691307a9f5f689ba08e8
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-01-16 11:28:31 +0100

    Pkgtemplates init

---
+++ src/SingleCellProjections.jl
@@ -0,0 +1,5 @@
+module SingleCellProjections
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SingleCellProjections
+using Test
+
+@testset "SingleCellProjections.jl" begin
+    # Write your tests here.
+end
