e8eebf91d99d5874a414465d71b4962f92b9e589
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-01-19 12:52:31 +0100

    Fix t-SNE glue code

--- src/tsne_glue.jl
+++ src/tsne_glue.jl
@@ -1,5 +1,5 @@
 function TSne.tsne(data::DataMatrix, args...; k=10, obs=:copy, kwargs...)
 	t = permutedims(TSne.tsne(obs_coordinates(data)', args...; kwargs...))
-	model = NearestNeighborModel("tsne",data.matrix, t, select(data.var,data.var_id_cols), k, "t-SNE", obs)
+	model = NearestNeighborModel("tsne", data, t; k, var="t-SNE", obs)
 	update_matrix(data, t, model; model.var, model.obs)
 end
