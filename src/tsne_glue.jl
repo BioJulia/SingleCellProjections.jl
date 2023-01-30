a77db28027feeef4eac77e6c273cdddbc2569251
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-01-30 15:47:53 +0100

    t-SNE docstring

--- src/tsne_glue.jl
+++ src/tsne_glue.jl
@@ -1,3 +1,16 @@
+"""
+	tsne(data::DataMatrix, args...; k=10, obs=:copy, kwargs...)
+
+Create a t-SNE embedding of `data`.
+Usually, `data` is a DataMatrix after reduction to `10-100` dimensions by `svd`.
+
+* `k` - The number of neighbors to use when projecting onto a t-SNE model. (Not used in the t-SNE computation, only during projection.)
+* `obs` - Can be `:copy` (make a copy of source `obs`) or `:keep` (share the source `obs` object).
+
+The other `args...` and `kwargs...` are forwarded to `TSne.tsne`. See `TSne` documentation for more details.
+
+See also: [`TSne.tsne`](@ref)
+"""
 function TSne.tsne(data::DataMatrix, args...; k=10, obs=:copy, kwargs...)
 	t = permutedims(TSne.tsne(obs_coordinates(data)', args...; kwargs...))
 	model = NearestNeighborModel("tsne", data, t; k, var="t-SNE", obs)
