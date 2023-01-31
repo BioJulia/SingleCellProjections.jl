36ae10b7697057d05cf9de918637aa663daaec0a
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-01-31 11:27:13 +0100

    obs_coordinates and var_coordinates docstrings

--- src/datamatrix.jl
+++ src/datamatrix.jl
@@ -89,6 +89,27 @@ Base.size(data::DataMatrix, dim::Integer) = size(data.matrix, dim)
 Base.axes(data::DataMatrix, d::Integer) = axes(data)[d] # needed to make end work in getindex
 
 
+"""
+	var_coordinates(data::DataMatrix)
+
+Returns a matrix with coordinates for the variables. Only available for DataMatrices that have a dual representation (e.g. SVD/PCA).
+
+In the case of `SVD` (PCA), `var_coordinates` returns the principal components as unit vectors.
+"""
+function var_coordinates end
+
+"""
+	obs_coordinates(data::DataMatrix)
+
+Returns a matrix with coordinates for the observations. Not available for all types of DataMatrices.
+Mostly useful for data matrices after dimension reduction such as `svd` or `force_layout` has been applied.
+
+In the case of `SVD` (PCA), `obs_coordinates` returns the principal components, scaled by the singular values.
+This is a a good starting point for downstream analysis, since it is the optimal linear approximation of the original data for the given number of dimensions.
+"""
+function obs_coordinates end
+
+
 var_coordinates(data::DataMatrix{<:Union{SVD,LowRank}}) = var_coordinates(data.matrix)
 obs_coordinates(data::DataMatrix{<:Union{SVD,LowRank}}) = obs_coordinates(data.matrix)
 obs_coordinates(data::DataMatrix{<:AbstractMatrix}) = data.matrix
