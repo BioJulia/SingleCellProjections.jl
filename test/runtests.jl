6b372f5322afaa2881627f6f9a81a3eb72277774
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-02-27 16:43:35 +0100

    Readd accidently removed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using SingleCellProjections
+using .SingleCellProjections.MatrixExpressions
 using Test
 using LinearAlgebra
 using SparseArrays
@@ -12,10 +13,10 @@ using SCTransform
 import SingleCellProjections: BarnesHutTree, build!
 
 
-# include("MatrixExpressions/runtests.jl")
+include("MatrixExpressions/runtests.jl")
 
 
 @testset "SingleCellProjections.jl" begin
     include("basic.jl")
-    # include("test_barnes_hut.jl")
+    include("test_barnes_hut.jl")
 end
