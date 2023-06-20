5131bdd0dec74550062f6d597bba40751fde342e
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-06-20 16:26:24 +0800

    mannwhitney improved docs

--- src/statistical_tests.jl
+++ src/statistical_tests.jl
@@ -44,6 +44,10 @@ _mannwhitney_table(ref::MatrixRef, args...; kwargs...) =
 	mannwhitney_table(data::DataMatrix, column, [groupA, groupB]; kwargs...)
 
 Perform a Mann-Whitney U-test (also known as a Wilcoxon rank-sum test) between two groups of observations.
+The U statistic is corrected for ties, and p-values are computed using a normal approximation.
+
+Note that `data` must be a `DataMatrix` containing a sparse matrix only.
+It is recommended to first [`logtransform`](@ref) (or [`tf_idf_transform`](@ref)) the raw counts before performing the Mann-Whitney U-test.
 
 `column` specifies a column in `data.obs` and is used to determine which observations belong in which group.
 Observations with `missing` values in the given `column` are always ignored.
@@ -75,6 +79,10 @@ end
 	mannwhitney!(data::DataMatrix, column, [groupA, groupB]; kwargs...)
 
 Perform a Mann-Whitney U-test (also known as a Wilcoxon rank-sum test) between two groups of observations.
+The U statistic is corrected for ties, and p-values are computed using a normal approximation.
+
+Note that `data` must be a `DataMatrix` containing a sparse matrix only.
+It is recommended to first [`logtransform`](@ref) (or [`tf_idf_transform`](@ref)) the raw counts before performing the Mann-Whitney U-test.
 
 `mannwhitney!` adds a U statistic and a p-value column to `data.var`.
 See [`mannwhitney_table`](@ref) for more details on groups and kwargs.
@@ -97,6 +105,10 @@ end
 	mannwhitney(data::DataMatrix, column, [groupA, groupB]; var=:copy, obs=:copy, matrix=:keep, kwargs...)
 
 Perform a Mann-Whitney U-test (also known as a Wilcoxon rank-sum test) between two groups of observations.
+The U statistic is corrected for ties, and p-values are computed using a normal approximation.
+
+Note that `data` must be a `DataMatrix` containing a sparse matrix only.
+It is recommended to first [`logtransform`](@ref) (or [`tf_idf_transform`](@ref)) the raw counts before performing the Mann-Whitney U-test.
 
 `mannwhitney` creates a copy of `data` and adds a U statistic and a p-value column to `data.var`.
 See [`mannwhitney!`](@ref) and [`mannwhitney_table`](@ref) for more details on groups and `kwargs`.
