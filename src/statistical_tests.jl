2f33d4c80a77e9706488f310927fc5890856064c
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-06-23 11:20:27 +0800

    ftest docstring
    Improve ftest_table docstring and add docstrings for ftest! and ftest.

--- src/statistical_tests.jl
+++ src/statistical_tests.jl
@@ -267,13 +267,13 @@ Performs an F-Test with the given `h1` (alternative hypothesis) and `h0` (null h
 Examples of F-Tests are ANOVA and Quadratic Regression, but any linear model can be used.
 (See "Examples" below for concrete examples.)
 
-F-tests can be performed on any DataMatrix, but it is almost always recommended to do it directly after transforming the data using e.g. `sctransform`, `logtransform` or `tf_idf_transform`.
+F-tests can be performed on any `DataMatrix`, but it is almost always recommended to do it directly after transforming the data using e.g. `sctransform`, `logtransform` or `tf_idf_transform`.
 
 !!! danger "Normalization"
-    Do not use `ftest_table` after normalizing the data using `normalize_matrix`, because `ftest_table` needs to know about the `h0` model (regressed out covariates) for correction computations. Failing to do so can result in incorrect results.
-    Instead, if you want to correct for the same covariates, pass them as `h0` to `ftest_table`.
+    Do not use `ftest_table` after normalizing the data using `normalize_matrix`: `ftest_table` needs to know about the `h0` model (regressed out covariates) for correction computations. Failing to do so can result in incorrect results.
+    If you want to correct for the same covariates, pass them as `h0` to `ftest_table`.
 
-`h1` can be
+`h1` can be:
 * A string specifying a column name of `data.obs`. Auto-detection determines if the column is categorical (ANOVA) or numerical.
 * A [`covariate`](@ref) for more control of how to interpret the values in a column.
 * A tuple or vector of the above for compound models.
@@ -281,34 +281,34 @@ F-tests can be performed on any DataMatrix, but it is almost always recommended 
 `ftest_table` returns a Dataframe with columns for variable IDs, F statistics and p-values.
 
 Supported `kwargs` are:
-* `h0`                  - Use a non-trivial H₀ (null) model. Specified in the same way as `h1` above. Defaults to intercept only.
+* `h0`                  - Use a non-trivial `h0` (null) model. Specified in the same way as `h1` above. Defaults to intercept only.
 * `statistic_col="F"`   - Name of the output column containing the F statistics. (Set to nothing to remove from output.)
 * `pvalue_col="pValue"` - Name of the output column containing the p-values. (Set to nothing to remove from output.)
-* `h1_missing=:skip` - One of `:skip` and `:error`. If `skip`, missing values in `h1` columns are skipped, otherwise an error is thrown.
-* `h0_missing=:error` - One of `:skip` and `:error`. If `skip`, missing values in `h0` columns are skipped, otherwise an error is thrown.
+* `h1_missing=:skip`    - One of `:skip` and `:error`. If `skip`, missing values in `h1` columns are skipped, otherwise an error is thrown.
+* `h0_missing=:error`   - One of `:skip` and `:error`. If `skip`, missing values in `h0` columns are skipped, otherwise an error is thrown.
 
 # Examples
 
 Perform an ANOVA using the "celltype" annotation.
 ```julia
-julia> ftest_table(data, "celltype")
+julia> ftest_table(transformed, "celltype")
 ```
 
-Perform an ANOVA using the "celltype" annotation, while eliminating `fraction_mt` (a linear covariate).
+Perform an ANOVA using the "celltype" annotation, while correcting for `fraction_mt` (a linear covariate).
 ```julia
-julia> ftest_table(data, "celltype"; h0="fraction_mt")
+julia> ftest_table(transformed, "celltype"; h0="fraction_mt")
 ```
 
-Perform an ANOVA using the "celltype" annotation, while eliminating `fraction_mt` (a linear covariate) and "phase" (a categorical covariate).
+Perform an ANOVA using the "celltype" annotation, while correcting for `fraction_mt` (a linear covariate) and "phase" (a categorical covariate).
 ```julia
-julia> ftest_table(data, "celltype"; h0=("fraction_mt","phase"))
+julia> ftest_table(transformed, "celltype"; h0=("fraction_mt","phase"))
 ```
 
-Perform Quadtractic Regression using the covariate `x`, by first creating an annotation for `x` squared, and then using a compound model.
+Perform Quadractic Regression using the covariate `x`, by first creating an annotation for `x` squared, and then using a compound model.
 ```julia
-julia> data.obs.x2 = data.obs.x.^2
+julia> data.obs.x2 = data.obs.x.^2;
 
-julia> ftest_table(data, ("x","x2"))
+julia> ftest_table(transformed, ("x","x2"))
 ```
 
 See also: [`ftest!`](@ref), [`ftest`](@ref), [`ttest_table`](@ref), [`covariate`](@ref)
@@ -327,6 +327,21 @@ function ftest_table(data::DataMatrix, h1;
 	_ftest_table(data, h1_design, h0_design; kwargs...)
 end
 
+"""
+	ftest!(data::DataMatrix, h1; h0, kwargs...)
+
+Performs an F-Test with the given `h1` (alternative hypothesis) and `h0` (null hypothesis).
+Examples of F-Tests are ANOVA and Quadratic Regression, but any linear model can be used.
+
+`ftest!` adds a F statistic and a p-value column to `data.var`.
+
+See [`ftest_table`](@ref) for usage examples and more details on computations and parameters.
+
+In addition `ftest!` supports the `kwarg`:
+* `prefix` - Output column names for F statistics and p-values will be prefixed with this string. If none is given, it will be constructed from `h1` and `h0`.
+
+See also: [`ftest_table`](@ref), [`ftest`](@ref), [`ttest!`](@ref)
+"""
 function ftest!(data::DataMatrix, h1;
                 h0=(),
                 prefix = _create_ftest_prefix(_splattable(h0), _splattable(h1)),
@@ -336,6 +351,18 @@ function ftest!(data::DataMatrix, h1;
 	data
 end
 
+"""
+	ftest(data::DataMatrix, h1; h0, kwargs...)
+
+Performs an F-Test with the given `h1` (alternative hypothesis) and `h0` (null hypothesis).
+Examples of F-Tests are ANOVA and Quadratic Regression, but any linear model can be used.
+
+`ftest!` creates a copy of `data` and adds a F statistic and a p-value column to `data.var`.
+
+See [`ftest!`](@ref) and [`ftest_table`](@ref)  for usage examples and more details on computations and parameters.
+
+See also: [`ftest!`](@ref), [`ftest_table`](@ref), [`ttest`](@ref)
+"""
 function ftest(data::DataMatrix, args...; var=:copy, obs=:copy, matrix=:keep, kwargs...)
 	data = copy(data; var, obs, matrix)
 	ftest!(data, args...; kwargs...)
