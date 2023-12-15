9de4ecdd440614a03a7c552281114112eafe4532
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2023-12-15 10:42:12 +0100

    Fix docstring for project(data, models)

--- src/datamatrix.jl
+++ src/datamatrix.jl
@@ -187,9 +187,9 @@ end
 
 
 """
-	project(data::DataMatrix, model::ProjectionModel, args...; verbose=true, kwargs...)
+	project(data::DataMatrix, models, args...; verbose=true, kwargs...)
 
-Convenience function for projection onto multiple models. Essentially calls `foldl` and prints some `@info` messages (if `verbose=true`).
+Convenience function for projection onto multiple `models`. Essentially calls `foldl` and prints some `@info` messages (if `verbose=true`).
 In most cases, it is better to call `project(data, base::DataMatrix)` instead of using this method directly.
 """
 function project(data::DataMatrix, models::AbstractVector{<:ProjectionModel}, args...; verbose=true, kwargs...)
