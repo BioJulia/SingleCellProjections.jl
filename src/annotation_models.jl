3f68ba20af32f21b21aeb339ee1d5ef22ebc7daf
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2024-12-20 11:12:32 +0800

    Minor fixes

--- src/annotation_models.jl
+++ src/annotation_models.jl
@@ -1,4 +1,4 @@
-struct AnnotationVectorModel <: ProjectionModel end
+struct ExtractAnnotationModel <: ProjectionModel end
 
 # No. This reordering should be a separate step to make projections work well.
 function _value_vector_reorder(obs_id::DataFrame, v)
@@ -19,18 +19,18 @@ function _value_vector_reorder(obs_id::DataFrame, v)
 end
 
 
-function project2(::AnnotationVectorModel, id::DataFrame, v)
+function project2(::ExtractAnnotationModel, id::DataFrame, v)
 	df = _get_table(v)
 
-	@assert size(obs_id,2) == 1
-	@assert names(obs_id,1) == names(df,1)
+	@assert size(id,2) == 1
+	@assert names(id,1) == names(df,1)
 	@assert size(df,2) == 2 # Do not allow multiple annotation columns, because then we couldn't return a Vector, and a Matrix doesn't work either because the eltype would be weird.
 
-	ind = indexin(obs_id[!,1], df[!,1])
+	ind = indexin(id[!,1], df[!,1])
 	if any(isnothing, ind)
 		n = count(isnothing, ind)
 		i = findfirst(isnothing,ind)
-		throw(ArgumentError("Error subsetting annotation, $n/$(size(df,1)) IDs are missing (first missing ID=\"$(obs_id[i,1]))\"."))
+		throw(ArgumentError("Error subsetting annotation, $n/$(size(df,1)) IDs are missing (first missing ID=\"$(id[i,1]))\"."))
 	end
 
 	ind = identity.(ind) # get rid of Nothings
