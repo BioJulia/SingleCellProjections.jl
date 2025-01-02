22f465a910b0ae01ed4c8a04283c4e5ea3ca6564
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-01-02 16:01:04 +0800

    Basic implementation of new filtering

--- src/filter2.jl
+++ src/filter2.jl
@@ -6,37 +6,33 @@ find_matching_ids(f, df::DataFrame) =
 find_matching_ids(::Colon, df::DataFrame) =
 	select(df, 1; copycols=false) # input is considered read-only, so we don't need to copy
 
-# find_matching_ids(f) = df->find_matching_ids(f, df)
 
-
-function subset_annotation(df::DataFrame, ids::DataFrame)
+function _get_id_ind(df::DataFrame, ids::DataFrame)
 	@assert size(ids, 2)==1
 	@assert names(df, 1) == names(ids, 1)
 
-	ind = indexin(df[!,1], ids[!,1])
+	ind = indexin(ids[!,1], df[!,1])
+	@show size(ind)
 	@assert all(!isnothing, ind) # TODO: Add boolean parameter allowing that only a subset is present
-	ind = identity.(ind) # remove Nothing from eltype
-	df[ind,:]
+	identity.(ind) # remove `Nothing` from eltype
+end
+_get_id_ind(df::DataFrame, ::Colon) = 1:size(df,1)
+
+function _subset_dataframe(df, ind)
+	# input is considered read-only, so only copy if we actually subset
+	ind == 1:size(df,1) ? df : df[ind, :]
 end
 
-function subset_datamatrix(data::DataMatrix, var_ids, obs_ids)
-	@assert size(var_ids, 2)==1
-	@assert size(obs_ids, 2)==1
-	@assert names(data.var, 1) == names(var_ids, 1)
-	@assert names(data.obs, 1) == names(obs_ids, 1)
+subset_annotation(df::DataFrame, ids) =
+	_subset_dataframe(df, _get_id_ind(df, ids))
 
-	var_ind = indexin(data.var[!,1], ids[!,1])
-	@assert all(!isnothing, var_ind) # TODO: Add boolean parameter allowing that only a subset is present
-	var_ind = identity.(var_ind) # remove Nothing from eltype
+function subset_matrix(data::DataMatrix, var_ids, obs_ids)
+	var_ind = _get_id_ind(data.var, var_ids)
+	obs_ind = _get_id_ind(data.obs, obs_ids)
 
-	obs_ind = indexin(data.obs[!,1], ids[!,1])
-	@assert all(!isnothing, obs_ind) # TODO: Add boolean parameter allowing that only a subset is present
-	obs_ind = identity.(obs_ind) # remove Nothing from eltype
+	var = _subset_dataframe(data.var, var_ind)
+	obs = _subset_dataframe(data.obs, obs_ind)
 
-	# TODO: Consider returning data.obs/var directly if var_ind/obs_ind == 1:N (instead of copying),
-	#       since inputs are considered read-only
 	matrix = _subsetmatrix(data.matrix, var_ind, obs_ind)
-	var = data.var[var_ind,:]
-	obs = data.obs[obs_ind,:]
 	DataMatrix(matrix, var, obs; duplicate_var=:ignore, duplicate_obs=:ignore)
 end
