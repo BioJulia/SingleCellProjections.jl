b5a7020c76d1b18a9c7d3f56b90b33ae15bab0e5
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-01-15 12:24:37 +0800

    CenteringModel2 fix

--- src/normalize2.jl
+++ src/normalize2.jl
@@ -1,12 +1,13 @@
 # TEMP simplified normalization for testing projections via Specs
 struct CenteringModel2 <: ProjectionModel
 	negβT::Matrix{Float64}
-end
-function CenteringModel2(A)
-	N = size(A,2)
-	X = ones(N,1)
-	negβT = (A*X) .* (-1/N)
-	CenteringModel2(negβT)
+
+	function CenteringModel2(A)
+		N = size(A,2)
+		X = ones(N,1)
+		negβT = (A*X) .* (-1/N)
+		new(negβT)
+	end
 end
 
 function project2(model::CenteringModel2, A)
