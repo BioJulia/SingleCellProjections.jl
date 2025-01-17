f63012c43cc0f39ebee40e193b83479ca4833238
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-01-17 14:15:05 +0800

    WIP new filtering

--- src/filter2.jl
+++ src/filter2.jl
@@ -7,7 +7,7 @@ find_matching_ids(::Colon, df::DataFrame) =
 	select(df, 1; copycols=false) # input is considered read-only, so we don't need to copy
 
 
-function _get_id_ind(df::DataFrame, ids::DataFrame)
+function ids_to_indices(df::DataFrame, ids::DataFrame)
 	@assert size(ids, 2)==1
 	@assert names(df, 1) == names(ids, 1)
 
@@ -16,23 +16,23 @@ function _get_id_ind(df::DataFrame, ids::DataFrame)
 	@assert all(!isnothing, ind) # TODO: Add boolean parameter allowing that only a subset is present
 	identity.(ind) # remove `Nothing` from eltype
 end
-_get_id_ind(df::DataFrame, ::Colon) = 1:size(df,1)
+ids_to_indices(df::DataFrame, ::Colon) = 1:size(df,1)
 
-function _subset_dataframe(df, ind)
-	# input is considered read-only, so only copy if we actually subset
-	ind == 1:size(df,1) ? df : df[ind, :]
+
+# The name is chosen since it is akin to getindex.
+function annotation_getindex(df::DataFrame, ind::Vector{Int})
+	if index_isnoop(ind,size(df,1))
+		df # input is considered read-only, so we don't need to copy
+	else
+		df[ind, :]
+	end
 end
 
-subset_annotation(df::DataFrame, ids) =
-	_subset_dataframe(df, _get_id_ind(df, ids))
-
-function subset_matrix(data::DataMatrix, var_ids, obs_ids)
-	var_ind = _get_id_ind(data.var, var_ids)
-	obs_ind = _get_id_ind(data.obs, obs_ids)
-
-	var = _subset_dataframe(data.var, var_ind)
-	obs = _subset_dataframe(data.obs, obs_ind)
-
-	matrix = _subsetmatrix(data.matrix, var_ind, obs_ind)
-	DataMatrix(matrix, var, obs; duplicate_var=:ignore, duplicate_obs=:ignore)
+# The name is chosen since it is akin to getindex
+function matrix_getindex(matrix, var_ind::Union{Vector{Int},Colon}, obs_ind::Union{Vector{Int},Colon})
+	if index_isnoop(var_ind, size(matrix,1)) && index_isnoop(obs_ind, size(matrix,2))
+		matrix # input is considered read-only, so we don't need to copy
+	else
+		_subsetmatrix(matrix, var_ind, obs_ind)
+	end
 end
