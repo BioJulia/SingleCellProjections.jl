4539bcee03e1e639421ccdf4eaf757ece15f039b
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-11-18 15:38:08 +0100

    Update matrix arithmetic specs

--- src/matrix_arithmetic.jl
+++ src/matrix_arithmetic.jl
@@ -7,38 +7,27 @@ _get_value(m::ReproducibleJobs.Managed{<:Pair}) = _get_value(ReproducibleJobs.un
 _get_value(x::Any) = x
 
 
-matrix_product_impl(args...) = SCPCore.matrixproduct(args...)
-matrix_product_projectable(action::Action, args...) =
-	create_spec(matrix_product_impl, action.(args)...; __version=v"0.1.1")
-matrix_product_projectable_spec(args...) =
-	create_spec(Projectable(matrix_product_projectable), args...)
-
+matrix_product_impl_spec(args...) =
+	create_spec(SCPCore.matrixproduct, args...; __version=v"0.1.1")
 
 # TODO: check that the inner var/obs IDs match!
-matrix_product(::Mat, args...) = matrix_product_projectable_spec(_map_value.(get_matrix_spec, args)...)
+matrix_product(::Mat, args...) = matrix_product_impl_spec(_map_value.(get_matrix_spec, args)...)
 matrix_product(::Var, args...) = get_var_spec(_get_value(first(args)))
 matrix_product(::Obs, args...) = get_obs_spec(_get_value(last(args)))
 
-
-function matrix_product_spec(args...)
-	isempty(args) && throw(ArgumentError("An empty matrix product is not allowed."))
-	create_spec(DataMatrixFunction(matrix_product), args...)
+function matrix_product_spec(arg1, args...)
+	create_spec(DataMatrixFunction(matrix_product), arg1, args...)
 end
 
-matrix_sum_impl(args...) = SCPCore.matrixsum(args...)
-matrix_sum_projectable(action::Action, args...) =
-	create_spec(matrix_sum_impl, action.(args)...; __version=v"0.1.1")
-matrix_sum_projectable_spec(args...) =
-	create_spec(Projectable(matrix_sum_projectable), args...)
 
+matrix_sum_impl_spec(args...) =
+	create_spec(SCPCore.matrixsum, args...; __version=v"0.1.1")
 
 # TODO: check that the inner var/obs IDs match!
-matrix_sum(::Mat, args...) = matrix_sum_projectable_spec(_map_value.(get_matrix_spec, args)...)
+matrix_sum(::Mat, args...) = matrix_sum_impl_spec(_map_value.(get_matrix_spec, args)...)
 matrix_sum(::Var, args...) = get_var_spec(_get_value(first(args)))
 matrix_sum(::Obs, args...) = get_obs_spec(_get_value(first(args)))
 
-
-function matrix_sum_spec(args...)
-	isempty(args) && throw(ArgumentError("An empty matrix sum is not allowed."))
-	create_spec(DataMatrixFunction(matrix_sum), args...)
+function matrix_sum_spec(arg1, args...)
+	create_spec(DataMatrixFunction(matrix_sum), arg1, args...)
 end
