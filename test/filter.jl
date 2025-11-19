6f7596cf7cb89e192d1bb200819efbc279d5dfb7
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-11-19 12:05:10 +0100

    Test filter forwarding

--- test/filter.jl
+++ test/filter.jl
@@ -29,29 +29,33 @@
 		P,N = size(data)
 
 		f_job = Jobs.filter_matrix(:, :, data_job)
-		@test forward(f_job).spec == data_spec_forwarded
 		let f = fetch!(f_job)
 			@test materialize(f) ≈ X
 			test_dataframe_columns_identical("f.var vs data.var", f.var, data.var)
 			test_dataframe_columns_identical("f.obs vs data.obs", f.obs, data.obs)
 		end
+		@test forward(f_job).spec == data_spec_forwarded
 
 		f_job = Jobs.filter_var(1:2:P, data_job)
-		@test forward(Jobs.get_obs(f_job)).spec == obs_spec_forwarded
 		let f = fetch!(f_job)
 			@test materialize(f) ≈ X[1:2:P, :]
 			@test isequal(f.var, data.var[1:2:P,:])
 			test_dataframe_columns_identical("f.obs vs data.obs", f.obs, data.obs)
 		end
+		@test forward(Jobs.get_obs(f_job)).spec == obs_spec_forwarded
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=1:2:P))
+		@test forward(f_job).spec == forward(ref_job).spec
 		# NB: Projection should fail unless the var IDs are identical!
 
 		f_job = Jobs.filter_obs(1:2:N, data_job)
-		@test forward(Jobs.get_var(f_job)).spec == var_spec_forwarded
 		let f = fetch!(f_job)
 			@test materialize(f) ≈ X[:, 1:2:N]
 			test_dataframe_columns_identical("f.var vs data.var", f.var, data.var)
 			@test isequal(f.obs, data.obs[1:2:N,:])
 		end
+		@test forward(Jobs.get_var(f_job)).spec == var_spec_forwarded
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; obs_ind=1:2:N))
+		@test forward(f_job).spec == forward(ref_job).spec
 		# NB: Projection should fail unless the obs IDs are identical!
 
 		f_job = Jobs.filter_matrix(1:3:P, 1:10:N, data_job)
@@ -60,6 +64,8 @@
 			@test isequal(f.var, data.var[1:3:P,:])
 			@test isequal(f.obs, data.obs[1:10:N,:])
 		end
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=1:3:P, obs_ind=1:10:N))
+		@test forward(f_job).spec == forward(ref_job).spec
 		# NB: Projection should fail unless the var and obs IDs are identical!
 
 		f_job = Jobs.filter_obs("group"=>==("A"), data_job)
@@ -69,6 +75,9 @@
 			test_dataframe_columns_identical("f.var vs data.var", f.var, data.var)
 			@test isequal(f.obs, filter("group"=>==("A"), data.obs))
 		end
+		ref_obs_ind = findall(==("A"), data.obs.group)
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; obs_ind=ref_obs_ind))
+		@test forward(f_job).spec == forward(ref_job).spec
 
 		# Hmm. How do we support this? The problem is the anonymous function that cannot be hashed currently. Implement HashableFunctions?
 		# f_job = Jobs.filter_obs(["group","value"]=>(g,v)->g=="A" && v<1.0, data_job)
@@ -85,16 +94,22 @@
 			@test isequal(f.var, data.var[1:3:P,:])
 			@test isequal(f.obs, data.obs[data.obs.group.=="A",:])
 		end
+		ref_obs_ind = findall(==("A"), data.obs.group)
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=1:3:P, obs_ind=ref_obs_ind))
+		@test forward(f_job).spec == forward(ref_job).spec
 		# NB: Projection should fail unless the var IDs are identical!
 
 
 		f_job = Jobs.filter_var("name"=>>("D"), data_job)
-		@test forward(Jobs.get_obs(f_job)).spec == obs_spec_forwarded
 		let f = fetch!(f_job)
 			@test materialize(f) ≈ X[data.var.name.>"D", :]
 			@test isequal(f.var, filter("name"=>>("D"), data.var))
 			test_dataframe_columns_identical("f.obs vs data.obs", f.obs, data.obs)
 		end
+		@test forward(Jobs.get_obs(f_job)).spec == obs_spec_forwarded
+		ref_var_ind = findall(>=("D"), data.var.name)
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=ref_var_ind))
+		@test forward(f_job).spec == forward(ref_job).spec
 
 		f_job = Jobs.filter_matrix("name"=>>("D"), 1:10:N, data_job)
 		let f = fetch!(f_job)
@@ -102,6 +117,9 @@
 			@test isequal(f.var, filter("name"=>>("D"), data.var))
 			@test isequal(f.obs, data.obs[1:10:N,:])
 		end
+		ref_var_ind = findall(>=("D"), data.var.name)
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=ref_var_ind, obs_ind=1:10:N))
+		@test forward(f_job).spec == forward(ref_job).spec
 		# NB: Projection should fail unless the obs IDs are identical!
 
 		f_job = Jobs.filter_matrix("name"=>>("D"), "group"=>==("A"), data_job)
@@ -110,6 +128,10 @@
 			@test isequal(f.var, filter("name"=>>("D"), data.var))
 			@test isequal(f.obs, filter("group"=>==("A"), data.obs))
 		end
+		ref_var_ind = findall(>=("D"), data.var.name)
+		ref_obs_ind = findall(==("A"), data.obs.group)
+		ref_job = ReproducibleJobs.Job(SingleCellProjections.create_datamatrix_getindex_spec(data_job; var_ind=ref_var_ind, obs_ind=ref_obs_ind))
+		@test forward(f_job).spec == forward(ref_job).spec
 
 
 		# annotations
