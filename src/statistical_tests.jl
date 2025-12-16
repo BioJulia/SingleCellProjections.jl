71b190aca54237ed1a99ba03f220b64d36ae0c44
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2025-12-16 14:39:11 +0100

    Move statistical test missing value handling to a separate function and use it also for ftests

--- src/statistical_tests.jl
+++ src/statistical_tests.jl
@@ -30,6 +30,39 @@ nonmissing_ind_spec(column_data...) =
 	create_spec(nonmissing_ind, column_data...; __version=v"0.1.0")
 
 
+
+function _filter_missing_obs(data, h::Tuple)
+	@assert !isempty(h) # should be handled before calling
+
+	# Handle missing values
+	skip_missing_cols = []
+	obs = get_obs_spec(data)
+
+	for a in h
+		if a isa Pair
+			a = a.first
+		end
+		push!(skip_missing_cols, _extract_data_spec(obs, a))
+	end
+
+	skip_missing_cols = (_extract_data_spec(obs, a isa Pair ? a.first : a) for a in h)
+	obs_ind = nonmissing_ind_spec(skip_missing_cols...)
+	create_datamatrix_getindex_spec(data; obs_ind)
+end
+
+function _filter_missing_obs(data; h1::Union{Tuple,AbstractVector}, h1_missing,
+                                   h0::Union{Tuple,AbstractVector}, h0_missing)
+	h1b = h1_missing == :skip ? h1 : ()
+	h0b = h0_missing == :skip ? h0 : ()
+	h = (h1b..., h0b...)
+	isempty(h) ? data : _filter_missing_obs(data, h)
+end
+
+
+
+
+
+
 function ftest_table_pr(action::Action, matrix, var_ids, h1_design, h0_design)
 	cached(create_spec(SCPCore.ftest_table2,
 	                   action(matrix), action(var_ids), action(h1_design), action(h0_design);
@@ -40,14 +73,18 @@ ftest_table_spec(matrix, var_ids, h1_design, h0_design) =
 	create_spec(Projectable(ftest_table_pr), matrix, var_ids, h1_design, h0_design)
 
 
-function ftest(::Preprocessing, data, h1; h0=(), center=true, max_categories=nothing)
-	# TODO: Filter observables with missing values (according to policy set in kwargs)
-
-	extra_kwargs = max_categories === nothing ? (;) : (; max_categories)
+function ftest(::Preprocessing, data, h1; h0=(), center=true, max_categories=nothing, h1_missing=:skip, h0_missing=:error)
+	@assert h1_missing in (:skip,:error)
+	@assert h0_missing in (:skip,:error)
 
 	# Wrap single hypothesis in tuples so we can splat them below
 	h1 = _splattable(h1)
 	h0 = _splattable(h0)
+
+	# Handle missing values
+	data = _filter_missing_obs(data; h1, h0, h1_missing, h0_missing)
+
+	extra_kwargs = max_categories === nothing ? (;) : (; max_categories)
 
 	# Hmm. We want h1 to be mean-zero (if center=true), but we don't want the intercept column.
 	h1_design = designmatrix_spec(data, h1...; center=false, extra_kwargs...)
@@ -82,78 +119,6 @@ ttest_table_spec(matrix, var_ids, h1_design, h1_scale, h0_design) =
 	create_spec(Projectable(ttest_table_pr), matrix, var_ids, h1_design, h1_scale, h0_design)
 
 
-# function ttest(::Preprocessing, data, h1; h0=(), center=true, max_categories=nothing, h1_missing=:skip, h0_missing=:error)
-# 	# Something like this
-# 	@assert h1_missing in (:skip,:error)
-# 	@assert h0_missing in (:skip,:error)
-
-# 	# # Filter observables with missing values if desired
-# 	# if h1_missing == :skip || h0_missing == :skip
-# 	#  or should this be done in covariate_stages?
-# 	#  nah, not possible, because we need for both h0 and h1 potentially
-# 	#  so we refactor to first get covariate_descriptions
-# 	#  then use that to filter if desired
-# 	#  	(NB: ensure `:` results in a no-op filter that just returns the parent datamatrix)
-# 	#  and to create design matrix
-# 	# end
-
-
-# 	# Wrap single hypothesis in tuples so we can splat them below
-# 	h0 = _splattable(h0)
-
-
-# 	h0_covariate_descriptions, center = setup_covariate_descriptions(h0...; center)
-
-# 	# TODO: We want h1 to be mean-zero (if center=true), but we don't want the intercept column. Fix.
-# 	h1_covariate_descriptions, _ = setup_covariate_descriptions(h1; center=false)
-
-# 	# Handle missing values
-# 	skip_missing_cols = []
-# 	let obs = get_obs_spec(data)
-# 		if h1_missing == :skip
-# 			col, desc = only(h1_covariate_descriptions)
-# 			push!(skip_missing_cols, _value_vector_data_spec(obs, col, desc))
-# 		end
-# 		if h0_missing == :skip
-# 			for (col,desc) in h0_covariate_descriptions
-# 				desc === SCPCore.intercept_covariate() && continue # an intercept does not have missing values
-# 				push!(skip_missing_cols, _value_vector_data_spec(obs, col, desc))
-# 			end
-# 		end
-# 	end
-
-# 	if !isempty(skip_missing_cols)
-# 		# @show skip_missing_cols
-# 		obs_ind = nonmissing_ind_spec(skip_missing_cols...)
-# 		data = create_datamatrix_getindex_spec(data; obs_ind)
-# 	end
-
-
-
-
-# 	extra_args = max_categories === nothing ? (;) : (; max_categories)
-
-
-# 	# We need to get the covariate model spec somewhere.
-# 	# Because it contains the scale info needed for the ttest (for difference).
-
-# 	obs = get_obs_spec(data)
-
-# 	(; covariate_model_specs, covariate_specs, covariate_names) = covariate_stages(obs, h1_covariate_descriptions; center=false, extra_args...)
-# 	h1_covariate_model = only(covariate_model_specs)
-# 	h1_scale = covariate_scale_spec(h1_covariate_model)
-# 	h1_design = build_designmatrix_spec(data, covariate_specs, covariate_names)
-
-# 	h0_design = designmatrix_spec(data, h0...; center, extra_args...) # TODO: Use h0_covariate_descriptions and build_design_matrix?
-
-# 	matrix = get_matrix_spec(data)
-# 	var_ids = id_column_spec(get_var_spec(data))
-
-# 	ttest_table_spec(matrix, var_ids, get_matrix_spec(h1_design), h1_scale, get_matrix_spec(h0_design))
-# end
-
-# function ttest_setup(::Preprocessing, data, h1; )
-
 
 # TODO: This does not work properly with projections. Fix.
 function ttest(::Preprocessing, data, h1; h0=(), center=true, max_categories=nothing, h1_missing=:skip, h0_missing=:error)
@@ -174,28 +139,7 @@ function ttest(::Preprocessing, data, h1; h0=(), center=true, max_categories=not
 
 
 	# Handle missing values
-	skip_missing_cols = []
-	let obs = get_obs_spec(data)
-		if h1_missing == :skip
-			push!(skip_missing_cols, _extract_data_spec(obs, h1.first))
-		end
-		if h0_missing == :skip
-			for a in h0
-				if a isa Pair
-					a = a.first
-				end
-				push!(skip_missing_cols, _extract_data_spec(obs, a))
-			end
-		end
-	end
-
-	if !isempty(skip_missing_cols)
-		# @show skip_missing_cols
-		obs_ind = nonmissing_ind_spec(skip_missing_cols...)
-		data = create_datamatrix_getindex_spec(data; obs_ind)
-	end
-
-
+	data = _filter_missing_obs(data; h1=_splattable(h1), h0, h1_missing, h0_missing)
 
 	obs = get_obs_spec(data)
 
