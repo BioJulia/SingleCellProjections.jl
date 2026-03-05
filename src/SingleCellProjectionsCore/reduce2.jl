a75a0a9ca918437b6e5c9e92a57fd06d4785c6ae
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2026-03-05 10:08:51 +0100

    Unwrap ReadOnlyArray

--- src/SingleCellProjectionsCore/reduce2.jl
+++ src/SingleCellProjectionsCore/reduce2.jl
@@ -2,6 +2,7 @@ function svd_projected_svt(U, X)
 	ΣV = X'U # TODO: compute U'X instead to get Vt directly
 	Matrix(ΣV')
 end
+svd_projected_svt(U::ReadOnlyArray, X) = svd_projected_svt(parent(U), X)
 
 svd_project_mul_sinv(ΣVt, S) = ΣVt ./ max.(S,1e-100) # To avoid NaNs if any singular value is zero
 
