020cd6087d56c0cf18de0ddf0c3df0190fd2e50e
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2026-03-06 20:43:19 +0100

    Readd mean_and_scale that was erroneously removed before

--- src/SingleCellProjectionsCore/design.jl
+++ src/SingleCellProjectionsCore/design.jl
@@ -1,3 +1,11 @@
+function mean_and_scale(v::AbstractVector; center)
+	m = center ? mean(v) : 0.0
+	# Consider using `norm` or `std` to rescale instead
+	s = max(1e-6, maximum(x->abs(x-m), v)) # Avoid scaling up if values are too small in absolute numbers
+	m, s
+end
+
+
 abstract type AbstractCovariateDesc end
 
 
