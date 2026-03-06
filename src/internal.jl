be4a4e211ed3c405803f6fd4bbdb6a636ac1b2dc
Rasmus Henningsson <rasmus.henningsson@gmail.com>
2026-03-06 11:13:42 +0100

    Remove old code

--- src/internal.jl
+++ src/internal.jl
@@ -209,11 +209,6 @@ function find_matching_ind(action::Action, f, df; project_ids::Symbol)
 	elseif f isa Pair
 		k = first(f)
 
-		# Removed in refactoring
-		# if k isa ReadOnly # Can we avoid this?
-		# 	k = k.value
-		# end
-
 		# subset the columns to only depend on those that are used
 		if k isa AbstractString
 			x = get_columns_spec(df, k)
